21e3e0b247bb7ff795a127bd36def93953979a1d
Adit Bhargava <adit.bhargava.school@gmail.com>
2025-11-21 16:49:49 -0500

    MIE243 portfolio integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,6 +20,7 @@ export default defineConfig({
                     { label: 'MIE243: Camera manipulator robot', link: '/projects/cameramanipulator' },
                     { label: 'NeuroTech Fall 2025: sEMG + exoskeleton for hand rehabilitation', link: '/projects/poststrokerehab' },
                     { label: 'EEG-based robotic actuation', link: '/projects/eegcontroller' },
+                    { label: 'MIE243 dissection: 90 degree speed reducer', link: '/projects/gearbox' },
                     //{ label: 'EEG-controller', link: '/projects/eegcontroller' }
                     // add more projects
                   ],
